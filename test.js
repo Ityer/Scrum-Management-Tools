0e89f9e707bc5552ea21e05478f226aa2562c08b
Ityer <andrewmackay1996@gmail.com>
2017-03-27 21:30:57 +0100

    Comments

--- test.js
+++ test.js
@@ -1,67 +1,66 @@
 if (localStorage.getItem("projects") === null) {document.location.href ='index.html';} //redirects to index if projects don't exist in localstorage
 
-var x = JSON.parse(localStorage.getItem('project'))
-var listOfProjects = JSON.parse(localStorage.getItem('projects'))
-project = {title:listOfProjects[x][0], description:listOfProjects[x][1], state:listOfProjects[x][2], stories:listOfProjects[x][3], tasks:listOfProjects[x][4], sprint:listOfProjects[x][5]};
-project.tasks.sort(sortFunction);
-function sortFunction(a, b) {
+var x = JSON.parse(localStorage.getItem('project')) //gets project id from local storage
+var listOfProjects = JSON.parse(localStorage.getItem('projects')) //gets all project data from local storage
+project = {title:listOfProjects[x][0], description:listOfProjects[x][1], state:listOfProjects[x][2], stories:listOfProjects[x][3], tasks:listOfProjects[x][4], sprint:listOfProjects[x][5]}; //puts active project into object for better readability
+project.tasks.sort(sortFunction); //sorts task in order of priority
+function sortFunction(a, b) {//function used by the sort function. It uses the priority attribute within a task to choose what order to list them in.
     if (a[3] === b[3]) {
-        return 0;
+        return 0; //returns 0 if priority of and b are identical
     }
     else {
-        return (a[3] < b[3]) ? -1 : 1;
+        return (a[3] < b[3]) ? -1 : 1; //returns stuff  
     }
 }
 
 
 
 function printStories(){
-	for (var i = 0; i < project.stories.length; i++) {
-		document.write("<div class=\"item\"><div><button class=\"editBtn\">⚙️<\/button><h2>"+project.stories[i][0]+"<\/h2><h5>Priority: "+project.stories[i][3]+"<\/h5><\/div><p><b>Description:<\/b> "+project.stories[i][1]+"<\/p><p><b>Definition of done:<\/b> "+project.stories[i][2]+"<\/p><\/div>");
+	for (var i = 0; i < project.stories.length; i++) { //for every story, write story
+		document.write("<div class=\"item\"><div><button class=\"editBtn\">⚙️<\/button><h2>"+project.stories[i][0]+"<\/h2><h5>Priority: "+project.stories[i][3]+"<\/h5><\/div><p><b>Description:<\/b> "+project.stories[i][1]+"<\/p><p><b>Definition of done:<\/b> "+project.stories[i][2]+"<\/p><\/div>");//custom div using story data
 	}
 }
 function printTasks(step,sprint){
-	for (var i = 0; i < project.tasks.length; i++) {
-		if ((project.tasks[i][5]==step)&&(project.tasks[i][6]==sprint)){
-			if(step != 4){
+	for (var i = 0; i < project.tasks.length; i++) { //for eery task
+		if ((project.tasks[i][5]==step)&&(project.tasks[i][6]==sprint)){ //if correct step and sprint
+			if(step != 4){ //if not last row, include next button
 				var next = "<button onclick=\"next(project.tasks["+i+"])\" class=\"editBtn\">➡️<\/button>";
-			}else{var next="";}
-			if(step != 1){
+			}else{var next="";}//make blank to avoid not defined error
+			if(step != 1){ //if not first row, include last button
 				var but = "<button onclick=\"last(project.tasks["+i+"])\" class=\"editBtn\">⬅️<\/button>";
-			}else{var but="";}
-			document.write("<div class=\"item\"><div id=\""+i+"\">"+next+but+"<h2>"+project.tasks[i][0]+"<\/h2><h5>Priority: "+project.tasks[i][3]+"<\/h5><h5>Length: "+project.tasks[i][4]+"<\/h5><\/div><p><b>Description:<\/b> "+project.tasks[i][1]+"<\/p><p><b>Definition of done:<\/b> "+project.tasks[i][2]+"<\/p><\/div>");
+			}else{var but="";}//make blank to avoid not defined error
+			document.write("<div class=\"item\"><div id=\""+i+"\">"+next+but+"<h2>"+project.tasks[i][0]+"<\/h2><h5>Priority: "+project.tasks[i][3]+"<\/h5><h5>Length: "+project.tasks[i][4]+"<\/h5><\/div><p><b>Description:<\/b> "+project.tasks[i][1]+"<\/p><p><b>Definition of done:<\/b> "+project.tasks[i][2]+"<\/p><\/div>");//custom div using task data
 			}
 	}
 }
 function save(){
-	listOfProjects[x][0] = project.title;
-	listOfProjects[x][1] = project.description;
-	listOfProjects[x][2] = project.state;
-	listOfProjects[x][3] = project.stories;
-	listOfProjects[x][4] = project.tasks;
-	listOfProjects[x][5] = project.sprint;
-	localStorage.setItem('projects',JSON.stringify(listOfProjects));
+	listOfProjects[x][0] = project.title;//take data from object and overwtire originaly stored data
+	listOfProjects[x][1] = project.description;//take data from object and overwtire originaly stored data
+	listOfProjects[x][2] = project.state;//take data from object and overwtire originaly stored data
+	listOfProjects[x][3] = project.stories;//take data from object and overwtire originaly stored data
+	listOfProjects[x][4] = project.tasks;//take data from object and overwtire originaly stored data
+	listOfProjects[x][5] = project.sprint;//take data from object and overwtire originaly stored data
+	localStorage.setItem('projects',JSON.stringify(listOfProjects));//save all projects in local storage
 }
-function saveExit(){
-	save()
-	document.location.href ='index.html';
+function saveExit(){ //when user clicks save and exit
+	save() //runs save
+	document.location.href ='index.html';//changes page to index
 }
 
-function strip(html)
-{
+function strip(html){//returns string with any html tags removed
    var tmp = document.createElement("DIV");
    tmp.innerHTML = html;
    return tmp.textContent || tmp.innerText || "";
 }
 
-function next(item){
+function next(item){//increases step by one, but not above 4. Saves and reloads to update on page
 	if (item[5] < 4){
 		item[5]+=1;
 		save()
 		location.reload();
 	}
 }
-function last(item){
+function last(item){//decreases step by one, but not below 1. Saves and reloads to update on page
 	if (item[5] > 1){
 		item[5]-=1;
 		save()
