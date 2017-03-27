2c25420065cd77d16f7f97ad11aaf3d8b2ccfc77
Ityer <andrewmackay1996@gmail.com>
2017-03-27 16:46:02 +0100

    many more changes
    Tasks ordered by priority
    test.html now redirects to intedx if no project has been stored in
    localstorage. tasks step can change.

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 //Story: title, body, dod, priority
 //task: title, body, dod, priority,time,step,sprint
-if (localStorage.getItem("projects") === null) {
-	console.log("New");
-	var projectOneStories = [["Story 1","Description of story 1","Definition of done1",1],["Story2","Description of story 2","Definition of done 2",2]];
-	var projectTwoStories = [["Story 1","Description of story 1","Definition of done1",3],["Story2","Description of story 2","Definition of done 2",4]];
-	var projectOneTasks = [["Task 1","Description of task 1","Definition of done 1",1,2,4,1],["Task 2","Description of task 2","Definition of done 2",1,3,3,1],["Task 3","Description of task 3","Definition of done 2",2,4,2,1],["Task 4","Description of task 4","Definition of done 4",3,5,1,1]];
-	var projectTwoTasks = [["Task 1","Description of task 5","Definition of done 1",1,6,4,1],["Task 2","Description of task 6","Definition of done 2",1,3,3,1],["Task 3","Description of task 7","Definition of done 2",2,8,2,1],["Task 4","Description of task 8","Definition of done 4",3,9,1,1]];
-	var listOfProjects = [["Project 1","Description of project 1",1,projectOneStories,projectOneTasks,1],["Project 2","Description of project 2",2,projectTwoStories,projectTwoTasks,1]];
-	localStorage.setItem("projects",JSON.stringify(listOfProjects));
+if (localStorage.getItem("projects") === null) { // if project files don't exist
+	var projectOneStories = [["Story 1","Description of story 1","Definition of done1",1],["Story2","Description of story 2","Definition of done 2",2]]; //sets up default stories
+	var projectTwoStories = [["Story 1","Description of story 1","Definition of done1",3],["Story2","Description of story 2","Definition of done 2",4]]; //sets up default stories
+	var projectOneTasks = [["Task 1","Description of task 1","Definition of done 1",1,2,4,1],["Task 2","Description of task 2","Definition of done 2",1,3,3,1],["Task 3","Description of task 3","Definition of done 2",2,4,2,1],["Task 4","Description of task 4","Definition of done 4",3,5,1,1]];//sets up default tasks
+	var projectTwoTasks = [["Task 1","Description of task 5","Definition of done 1",1,6,4,1],["Task 2","Description of task 6","Definition of done 2",1,3,3,1],["Task 3","Description of task 7","Definition of done 2",2,8,2,1],["Task 4","Description of task 8","Definition of done 4",3,9,1,1]];//sets up default tasks
+	var listOfProjects = [["Project 1","Description of project 1",1,projectOneStories,projectOneTasks,1],["Project 2","Description of project 2",2,projectTwoStories,projectTwoTasks,1]];//sets up default projects
+	localStorage.setItem("projects",JSON.stringify(listOfProjects)); //save default projects to localstorage. While not entirely dependable, localstorage will persist between sessions.
 }
-else{
-	console.log("From Local");
-	var listOfProjects = JSON.parse(localStorage.getItem('projects'))
+else{//if projects already exist
+	var listOfProjects = JSON.parse(localStorage.getItem('projects'))//load projects]
 }
-var bgcolor = "white";
-for (var i = 0; i < listOfProjects.length; i++) {
-	switch( listOfProjects[i][2]) {
+var bgcolor = "white"; //default colour
+for (var i = 0; i < listOfProjects.length; i++) { //cycle through projects
+	switch( listOfProjects[i][2]) { //check if ontime or behind
 		case 1:
-			bgcolor = "green";
+			bgcolor = "green"; //green for ontime
 			break;
 		case 2:
-			bgcolor = "red";
+			bgcolor = "red"; //red for behind
 			break;
 	}
 	listProject = "<div id=\""+(i)+"\" class=\"project\" style = \"background-color: "+bgcolor+"\"><h2>" + listOfProjects[i][0] + "<\/h2><h3>" + listOfProjects[i][1] + "<\/h3><\/div>";
--- test.js
+++ test.js
@@ -1,6 +1,20 @@
+if (localStorage.getItem("projects") === null) {document.location.href ='index.html';} //redirects to index if projects don't exist in localstorage
+
 var x = JSON.parse(localStorage.getItem('project'))
 var listOfProjects = JSON.parse(localStorage.getItem('projects'))
 project = {title:listOfProjects[x][0], description:listOfProjects[x][1], state:listOfProjects[x][2], stories:listOfProjects[x][3], tasks:listOfProjects[x][4], sprint:listOfProjects[x][5]};
+project.tasks.sort(sortFunction);
+function sortFunction(a, b) {
+    if (a[3] === b[3]) {
+        return 0;
+    }
+    else {
+        return (a[3] < b[3]) ? -1 : 1;
+    }
+}
+
+
+
 function printStories(){
 	for (var i = 0; i < project.stories.length; i++) {
 		document.write("<div class=\"item\"><div><button class=\"editBtn\">⚙️<\/button><h2>"+project.stories[i][0]+"<\/h2><h5>Priority: "+project.stories[i][3]+"<\/h5><\/div><p><b>Description:<\/b> "+project.stories[i][1]+"<\/p><p><b>Definition of done:<\/b> "+project.stories[i][2]+"<\/p><\/div>");
@@ -9,12 +23,17 @@ function printStories(){
 function printTasks(step,sprint){
 	for (var i = 0; i < project.tasks.length; i++) {
 		if ((project.tasks[i][5]==step)&&(project.tasks[i][6]==sprint)){
-			document.write("<div class=\"item\"><div><button class=\"editBtn\">⚙️<\/button><h2>"+project.tasks[i][0]+"<\/h2><h5>Priority: "+project.tasks[i][3]+"<\/h5><h5>Length: "+project.tasks[i][4]+"<\/h5><\/div><p><b>Description:<\/b> "+project.tasks[i][1]+"<\/p><p><b>Definition of done:<\/b> "+project.tasks[i][2]+"<\/p><\/div>");
-		}
+			if(step != 4){
+				var next = "<button onclick=\"next(project.tasks["+i+"])\" class=\"editBtn\">➡️<\/button>";
+			}else{var next="";}
+			if(step != 1){
+				var but = "<button onclick=\"last(project.tasks["+i+"])\" class=\"editBtn\">⬅️<\/button>";
+			}else{var but="";}
+			document.write("<div class=\"item\"><div id=\""+i+"\">"+next+but+"<h2>"+project.tasks[i][0]+"<\/h2><h5>Priority: "+project.tasks[i][3]+"<\/h5><h5>Length: "+project.tasks[i][4]+"<\/h5><\/div><p><b>Description:<\/b> "+project.tasks[i][1]+"<\/p><p><b>Definition of done:<\/b> "+project.tasks[i][2]+"<\/p><\/div>");
+			}
 	}
 }
-
-function saveExit(){
+function save(){
 	listOfProjects[x][0] = project.title;
 	listOfProjects[x][1] = project.description;
 	listOfProjects[x][2] = project.state;
@@ -22,6 +41,9 @@ function saveExit(){
 	listOfProjects[x][4] = project.tasks;
 	listOfProjects[x][5] = project.sprint;
 	localStorage.setItem('projects',JSON.stringify(listOfProjects));
+}
+function saveExit(){
+	save()
 	document.location.href ='index.html';
 }
 
@@ -31,3 +53,19 @@ function strip(html)
    tmp.innerHTML = html;
    return tmp.textContent || tmp.innerText || "";
 }
+
+function next(item){
+	if (item[5] < 4){
+		item[5]+=1;
+		save()
+		location.reload();
+	}
+}
+function last(item){
+	if (item[5] > 1){
+		item[5]-=1;
+		save()
+		location.reload();
+	}
+}
+
